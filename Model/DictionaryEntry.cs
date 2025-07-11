d5f2f6ce8c3d5283e67aab6dcf7de6e18bb4cb8f
Oleksandr <oleksandrhelpa@gmail.com>
2025-07-12 00:37:58 +0300

    update data model

---
+++ Model/DictionaryEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currencyConverter.Model
+{
+    public class DictionaryEntry
+    {
+        private int id;
+        private string code;
+        private string description;
+        
+        public int Id
+        {
+            get => id;
+            
+        }
+        public string Code
+        {
+            get => code;
+        }
+        public string Description
+        {
+            get => description;
+        }
+    }
+}
