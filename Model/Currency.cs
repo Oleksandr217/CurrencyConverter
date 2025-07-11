9b2d8122ae9a462863cff7ca20c9b072ef65ed45
Oleksandr <oleksandrhelpa@gmail.com>
2025-07-11 20:13:18 +0300

    add data model

---
+++ Model/Currency.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currencyConverter.Model
+{
+    public class Currency : INotifyPropertyChanged
+    {
+        private string name;
+        private int rateToUSD;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        public int RateToUSD
+        {
+            get => rateToUSD;
+            set
+            {
+                rateToUSD = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
